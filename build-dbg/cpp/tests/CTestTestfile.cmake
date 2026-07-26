ffda06de1e0a8603e97edb7f0cf7c642f3606e48
vangee <vangeechen@outlook.com>
2026-07-27 00:38:54 +0800

    chore: untrack build directories (gitignore build*/)

--- build-dbg/cpp/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/WorkSpace/language-Frontier/cpp/tests
-# Build directory: H:/WorkSpace/language-Frontier/build-dbg/cpp/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[frontier_cpp_tests]=] "H:/WorkSpace/language-Frontier/build-dbg/cpp/tests/frontier_tests.exe")
-set_tests_properties([=[frontier_cpp_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "H:/WorkSpace/language-Frontier/cpp/tests/CMakeLists.txt;15;add_test;H:/WorkSpace/language-Frontier/cpp/tests/CMakeLists.txt;0;")
--- build-msvc/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: H:/WorkSpace/language-Frontier
-# Build directory: H:/WorkSpace/language-Frontier/build-msvc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cpp")
-subdirs("bindings")
-subdirs("cpp/tests")
